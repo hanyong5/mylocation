5fab1c61ea497c03fb0e630664267e2d7987cc08
hanyong5 <hanyong5@naver.com>
2025-04-30 14:13:48 +0900

    location#1

--- vite.config.js
+++ vite.config.js
@@ -2,23 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   base: '/', // 꼭 이 값을 설정해야 Netlify에서 정적 자산 경로를 제대로 인식함
-  server: {
-    fs: {
-      strict: false,
-    },
-  },
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: undefined,
-      },
-    },
-  },
-  optimizeDeps: {
-    include: ['react-kakao-maps-sdk'],
-  },
 });
